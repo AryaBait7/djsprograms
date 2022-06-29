1fb625a01fb240289d74a1bdc2f28601eb03055c
arya <deepabait26577@gmail.com>
2022-06-30 00:41:34 +0530

    init

--- prog12.c
+++ prog12.c
@@ -2,10 +2,6 @@
 #include<conio.h>
 #include<math.h>
 int main(){
-    int n,r;
-    printf("Enter value of n:");
-    scanf("%d",&n);
-    printf("Enter value of r");
-    scanf("%d",&r);
+    // Same as Q.11
 
 }
